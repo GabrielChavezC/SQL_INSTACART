09716a48d7218b55366bf05911ab7a6bf02fa582
Gabriel Chavez Camargo <gabriel_chavez_ds@hotmail.com>
2024-10-03 15:26:12 -0600

    correcion_grafico

--- preprocessing/a02_gabriel.py
+++ preprocessing/a02_gabriel.py
@@ -41,17 +41,16 @@ plt.show()
 
 
 
-#7. ¿Cuáles son los 20 principales artículos que vuelven a pedirse con mayor frecuencia (muestra sus nombres e IDs de los productos)?
-
+#7. ¿Cuántos artículos compran normalmente las personas en un pedido? ¿Cómo es la distribución?
 query='''
-SELECT order_count, COUNT(*) AS num_users
+SELECT product_count, COUNT(*) AS num_orders
 FROM (
-    SELECT user_id, COUNT(order_id) AS order_count
-    FROM instacart_orders
-    GROUP BY user_id
+    SELECT order_id, COUNT(product_id) AS product_count
+    FROM order_products
+    GROUP BY order_id
 ) AS subquery
-GROUP BY order_count
-ORDER BY num_users DESC;
+GROUP BY product_count
+ORDER BY product_count ASC;
 
 '''
 
@@ -60,9 +59,10 @@ ejemplo=pd.read_sql_query(query,conn)
 print(ejemplo)
 
 
-ejemplo.plot(title="Hora del pedido",
+ejemplo.plot(title="Artículos suelen comprar las personas en un pedido",
                         kind="bar",
                         xlabel="Pedidos",
+                         figsize=[15, 6],
                          ylabel="Clientes",
                          legend=False)
 plt.show()
