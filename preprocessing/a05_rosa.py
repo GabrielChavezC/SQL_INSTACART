b05ab24f98e3bd0ef8d4a074fad5223d63cfc467
RosaIsela <rosaiselagonza72@gmail.com>
2024-10-03 19:44:49 -0600

    agregue pd.read

--- preprocessing/a05_rosa.py
+++ preprocessing/a05_rosa.py
@@ -19,13 +19,9 @@ GROUP BY p.product_id, p.product_name
 ORDER BY count_products DESC
 LIMIT 20
 '''
+graf_6 = pd.read_sql_query(query_6, conn)
+print(graf_6)
 
-cursor = conn.cursor()  
-cursor.execute(query_6)    
-results = cursor.fetchall() 
-
-for row in results:
-    print(f"ID: {row[0]}, Producto: {row[1]}, Cantidad: {row[2]}")
 
 # 11.¿Cuáles son los 20 principales artículos que las personas ponen primero en sus carritos?
 
@@ -37,12 +33,8 @@ GROUP BY o.product_id
 ORDER BY add_to_cart_order DESC
 LIMIT 20
 '''
-cursor = conn.cursor()  # Crear un cursor para ejecutar la consulta
-cursor.execute(query_11)    # Ejecutar la consulta
-results = cursor.fetchall()  # Obtener todos los resultados
-
-for row in results:
-    print(f"Producto: {row[0]}")
+graf_11 = pd.read_sql_query(query_11, conn)
+print(graf_11)
 
 # 4. Diferencia entre miércoles y sábados para 'order_hour_of_day'. Traza gráficos de barra para los dos días y describe las diferencias que veas.
 
@@ -60,24 +52,17 @@ ORDER BY order_hour_of_day
 graf_4 = pd.read_sql_query(query_4, conn)
 print(graf_4)
 
-# Ejecutar la consulta
-cursor = conn.cursor()
-cursor.execute(query_4)
-
-# Cargar los resultados en un DataFrame
-data = cursor.fetchall()
-df = pd.DataFrame(data, columns=['order_hour_of_day', 'order_count', 'order_dow'])
 
 # Convertir 'order_dow' a un nombre legible
-df['day_of_week'] = df['order_dow'].replace({'3': 'Wednesday', '6': 'Saturday'})
-df['order_hour_of_day'] = df['order_hour_of_day'].astype(int)  # Convertir a int
+graf_4['day_of_week'] = graf_4['order_dow'].replace({'3': 'Wednesday', '6': 'Saturday'})
+graf_4['order_hour_of_day'] = graf_4['order_hour_of_day'].astype(int)  # Convertir a int
 
 
 
 # Gráfico de barras
 graf_4.plt.figure(figsize=(12, 6))
-for day in df['day_of_week'].unique():
-    subset = df[df['day_of_week'] == day]
+for day in graf_4['day_of_week'].unique():
+    subset = graf_4[graf_4['day_of_week'] == day]
     plt.bar(subset['order_hour_of_day'] + (0.2 if day == 'Wednesday' else -0.2), 
             subset['order_count'], 
             width=0.4, 
